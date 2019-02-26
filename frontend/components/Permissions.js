0511fc6edf01d2d1af66bf2bb68887d16cee95c8
Cavan Wagg <waggx002@gmail.com>
2019-02-25 20:06:24 -0600

    permissions management part 2

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -3,6 +3,16 @@ import Error from './ErrorMessage';
 import gql from 'graphql-tag';
 import Table from './styles/Table';
 import PropTypes from 'prop-types';
+import SickButton from './styles/SickButton';
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE'
+];
 
 const ALL_USERS_QUERY = gql`
   query {
@@ -17,26 +27,52 @@ const ALL_USERS_QUERY = gql`
 
 const Permissions = props => (
   <Query query={ALL_USERS_QUERY}>
-    {({ data, loading, error }) =>
-      console.log(data) || (
+    {({ data, loading, error }) => (
+      <div>
+        <Error error={error} />
         <div>
-          <Error error={error} />
-          <div>
-            <h2>Manage Permissions</h2>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>{data.users.map(user => user.name)}</tbody>
-            </Table>
-          </div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(permission => (
+                  <th>{permission}</th>
+                ))}
+                <th>👇</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data && data.users && data.users.map(user => user.name)}
+            </tbody>
+          </Table>
         </div>
-      )
-    }
+      </div>
+    )}
   </Query>
 );
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user;
+    return (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        {possiblePermissions.map(permission => (
+          <td>
+            <label htmlFor={`${user.id}-permission-${permission}`}>
+              <input type="checkbox" />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    );
+  }
+}
 
 export default Permissions;
