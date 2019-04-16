cac149af65575280cc41fcbdd785d774c7a2b6a4
Cavan Wagg <waggx002@gmail.com>
2019-04-15 19:47:45 -0500

    More Apollo Query Testing

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -9,7 +9,7 @@ const PleaseSignIn = props => (
       if (!data.me) {
         return (
           <div>
-            <p>Please Sign In</p>
+            <p>Please Sign In before Continuing</p>
             <Signin />
           </div>
         );
