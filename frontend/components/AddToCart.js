33914c46c8bfab59bee8ef162ca8b5c34016c4a9
Cavan Wagg <waggx002@gmail.com>
2019-10-13 19:39:46 -0500

    Testing our cart

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -4,24 +4,33 @@ import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
-   mutation addToCart($id: ID!) {
-     addToCart(id: $id) {
-       id
-       quantity
-     }
-   }
+  mutation addToCart($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
 `;
 
 class AddToCart extends React.Component {
   render() {
     const { id } = this.props;
-    return (<Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id, }}
-      refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-    >
-      {(addToCart, { loading }) => <button disabled={loading} onClick={addToCart}>Add{loading && 'ing'} to Cart 🛒 </button>}
-    </Mutation>
+    return (
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{
+          id,
+        }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart 🛒
+          </button>
+        )}
+      </Mutation>
     );
   }
 }
-
 export default AddToCart;
+export { ADD_TO_CART_MUTATION };
